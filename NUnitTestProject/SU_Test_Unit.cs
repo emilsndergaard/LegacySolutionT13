8ccc24df34277815901ed10d55a55146cd655708
SofieUhre <60734443+SofieUhre@users.noreply.github.com>
2021-02-25 13:53:18 +0100

    Tilføjer SU_Unit_Test

---
+++ NUnitTestProject/SU_Test_Unit.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LegacySolutionT13;
+using NUnit.Framework;
+using NSubstitute;
+
+namespace LegacySolutionT13.UnitTest
+{
+    class SU_Test_Unit
+    {
+        private IHeater uut;
+        private ILogger fakeLogger;
+
+        [SetUp]
+        public void Setup()
+        {
+            fakeLogger = Substitute.For<ILogger>();
+            uut = new Heater(fakeLogger);
+        }
+
+        [Test]
+        public void TurnOn_CallMethodTurnOn_ResultIsTrue()
+        {
+            uut.TurnOn();
+            fakeLogger.Received(1).PrintOn();
+        }
+
+        [Test]
+        public void TurnOff_CallMethodTurnOff_ResultIsTrue()
+        {
+            uut.TurnOff();
+            fakeLogger.Received(1).PrintOff();
+        }
+        [Test]
+        public void SelfTest_CallMethodSelfTest_ResultIsTrue()
+        {
+            uut.RunSelfTest();
+            Assert.IsTrue(uut.RunSelfTest());
+        }
+
+    }
+
+  
+}
+
